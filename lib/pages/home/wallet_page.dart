e50df16222bdf6ce8b0ae36747aabadc1807dd5b
finma <firmanmaulana2403@gmail.com>
2023-08-28 11:21:19 +0700

    [ADD] wallet page

---
+++ lib/pages/home/wallet_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class WalletPage extends StatelessWidget {
+  const WalletPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Wallet Page',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
