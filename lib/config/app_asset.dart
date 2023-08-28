f9b0c9adc9027f290aa6cf2587f714854e1d95cf
finma <firmanmaulana2403@gmail.com>
2023-08-28 10:36:18 +0700

    [ADD] app asset

---
+++ lib/config/app_asset.dart
@@ -0,0 +1,36 @@
+class AppAsset {
+  static const animSearching = 'assets/animations/searching.json';
+  static const fotoBanner = 'assets/images/foto_banner.png';
+  static const fotoBox = 'assets/images/foto_box.png';
+  static const fotoPayload = 'assets/images/foto_payload.png';
+  static const fotoVehiclePickup = 'assets/images/foto_vehicle_pickup.png';
+  static const fotoVehicleTruck = 'assets/images/foto_vehicle_truck.png';
+  static const fotoVehicleTruckBox = 'assets/images/foto_vehicle_truck_box.png';
+  static const iconBag = 'assets/images/icon_bag.png';
+  static const iconCall = 'assets/images/icon_call.png';
+  static const iconClock = 'assets/images/icon_clock.png';
+  static const iconDocument = 'assets/images/icon_document.png';
+  static const iconExclamationMark = 'assets/images/icon_exclamation_mark.png';
+  static const iconFlag = 'assets/images/icon_flag.png';
+  static const iconGps = 'assets/images/icon_gps.png';
+  static const iconHome = 'assets/images/icon_home.png';
+  static const iconLocation = 'assets/images/icon_location.png';
+  static const iconLock = 'assets/images/icon_lock.png';
+  static const iconLogout = 'assets/images/icon_logout.png';
+  static const iconLove = 'assets/images/icon_love.png';
+  static const iconMail = 'assets/images/icon_mail.png';
+  static const iconMessage = 'assets/images/icon_message.png';
+  static const iconNotification = 'assets/images/icon_notification.png';
+  static const iconPeople = 'assets/images/icon_people.png';
+  static const iconPoint = 'assets/images/icon_point.png';
+  static const iconProfile = 'assets/images/icon_profile.png';
+  static const iconQuestionMark = 'assets/images/icon_question_mark.png';
+  static const iconSetting = 'assets/images/icon_setting.png';
+  static const iconStar = 'assets/images/icon_star.png';
+  static const iconTruck = 'assets/images/icon_truck.png';
+  static const iconWallet = 'assets/images/icon_wallet.png';
+  static const logoDeliverit = 'assets/images/logo_deliverit.png';
+  static const logoDeliveritText = 'assets/images/logo_deliverit_text.png';
+  static const logoGoogle = 'assets/images/logo_google.png';
+  static const profile = 'assets/images/profile.jpg';
+}
