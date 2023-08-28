9671c47068004e1f4b244ee07b07b5a832c51b63
finma <firmanmaulana2403@gmail.com>
2023-08-28 10:36:27 +0700

    [ADD] app color

---
+++ lib/config/app_color.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/animation.dart';
+
+class AppColor {
+  static const primary = Color(0xff4A43EC);
+  static const secondary = Color(0xffD2DEEE);
+
+  static const gradient = Color(0xffF8D1D1);
+  static const blue = Color(0xff02E9FE);
+}
