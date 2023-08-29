31b3849fe87e9826438273bfc175eabde3938821
finma <firmanmaulana2403@gmail.com>
2023-08-29 11:01:22 +0700

    [ADD] user model

---
+++ lib/models/user.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_database/firebase_database.dart';
+
+class User {
+  late String id;
+  late String email;
+  late String name;
+  late int phoneNumber;
+
+  User({
+    required this.id,
+    required this.email,
+    required this.name,
+    required this.phoneNumber,
+  });
+
+  User.fromSnapshot(DataSnapshot dataSnapshot) {
+    id = dataSnapshot.key!;
+    final Map<dynamic, dynamic>? data =
+        dataSnapshot.value as Map<dynamic, dynamic>?;
+
+    if (data != null) {
+      email = data['email'];
+      name = data['name'];
+      phoneNumber = data['phoneNumber'];
+    }
+  }
+}
