c52fce58b8cc5fb8edb42c382fc396536970f197
finma <firmanmaulana2403@gmail.com>
2023-09-04 22:20:48 +0700

    [ADD] driver model

--- lib/config/map_config.dart
+++ lib/config/map_config.dart
@@ -1,6 +1,9 @@
 import 'package:assets_audio_player/assets_audio_player.dart';
 
+import '/models/driver.dart';
+
 String mapKey = 'AIzaSyAb0VVTubNOlT-M8p4L5FyZnJoR9unOnxs';
 String oldMapKey = 'AIzaSyDPaBgt_oedAQiUxlApwiPc9LCsx7W96pQ';
 
 AssetsAudioPlayer audioPlayer = AssetsAudioPlayer();
+Driver driverInformation = Driver();
