668e589ff11f2fa4f453ace0b6b7f05de6695dc7
finma <firmanmaulana2403@gmail.com>
2023-09-04 17:29:45 +0700

    [ADD] ride request ref

--- lib/config/firebase.dart
+++ lib/config/firebase.dart
@@ -1,3 +1,5 @@
 import 'package:firebase_database/firebase_database.dart';
 
 DatabaseReference driverRef = FirebaseDatabase.instance.ref().child('drivers');
+DatabaseReference newRequestRef =
+    FirebaseDatabase.instance.ref().child('rideRequests');
