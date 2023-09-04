18aa13637ab2461041b218c916a4689d5523d67d
finma <firmanmaulana2403@gmail.com>
2023-09-04 20:56:57 +0700

    [ADD] audio player

--- lib/config/map_config.dart
+++ lib/config/map_config.dart
@@ -1,2 +1,6 @@
+import 'package:assets_audio_player/assets_audio_player.dart';
+
 String mapKey = 'AIzaSyAb0VVTubNOlT-M8p4L5FyZnJoR9unOnxs';
 String oldMapKey = 'AIzaSyDPaBgt_oedAQiUxlApwiPc9LCsx7W96pQ';
+
+AssetsAudioPlayer audioPlayer = AssetsAudioPlayer();
