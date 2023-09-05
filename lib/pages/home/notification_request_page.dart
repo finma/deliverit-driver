e65ce52ac7128fc9966f75f3c5aa8c18607024f3
finma <firmanmaulana2403@gmail.com>
2023-09-05 11:48:29 +0700

    [ADD] google map and draw polyline points

--- lib/pages/home/notification_request_page.dart
+++ lib/pages/home/notification_request_page.dart
@@ -1,7 +1,13 @@
+import 'dart:async';
+
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '/config/app_asset.dart';
@@ -16,91 +22,267 @@ import '/data/vehicle.dart';
 import '/models/payload.dart';
 import '/models/ride_details.dart';
 import '/models/vehicle.dart';
+import '/services/googlemap.dart';
 
-class NotificationRidePage extends StatelessWidget {
+// ignore: must_be_immutable
+class NotificationRidePage extends HookWidget {
   final RideDetails rideDetails;
 
   NotificationRidePage({super.key, required this.rideDetails});
+
+  late GoogleMapController newRideGoogleMapController;
 
   final payloads = DataPayload.all;
   final vehicles = DataVehicle.all;
 
   final double bottomSheetHeight = 160;
+  final CameraPosition initialCameraPosition = const CameraPosition(
+    target: LatLng(-7.319563, 108.202972),
+    zoom: 14.4746,
+  );
 
   @override
   Widget build(BuildContext context) {
+    //* show google map when ride request accepted
+    final isShowGoogleMap = useState(false);
+    final isDirectionLoaded = useState(false);
+
+    final Completer<GoogleMapController> controllerGoogleMap =
+        Completer<GoogleMapController>();
+
+    final markerSet = useState<Set<Marker>>(<Marker>{});
+    final polylineSet = useState<Set<Polyline>>(<Polyline>{});
+    final circleSet = useState<Set<Circle>>(<Circle>{});
+    final polylineCoordinates = useState<List<LatLng>>(<LatLng>[]);
+
+    // * GET PLACE DIRECTION AND DRAW ROUTE ON MAP
+    Future<void> getPlaceDirection(
+      LatLng pickupLatLng,
+      LatLng dropoffLatLng,
+    ) async {
+      // * GET PICK UP AND DROP OFF ADDRESS
+      LatLng pickUpLatLng =
+          LatLng(pickupLatLng.latitude, pickupLatLng.longitude);
+      LatLng dropOffLatLng =
+          LatLng(dropoffLatLng.latitude, dropoffLatLng.longitude);
+
+      //* GET DIRECTION DETAILS
+      var details = await GoogleMapService.obtainPlaceDirectionDetails(
+          pickUpLatLng, dropOffLatLng);
+
+      // * DECODE POLYLINE POINTS
+      PolylinePoints polylinePoints = PolylinePoints();
+      List<PointLatLng> decodedPolylinePointsResult =
+          polylinePoints.decodePolyline(details.encodedPoints!);
+
+      // * ADD DECODED POLYLINE POINTS TO ARRAY
+      polylineCoordinates.value.clear();
+      if (decodedPolylinePointsResult.isNotEmpty) {
+        for (var pointLatLng in decodedPolylinePointsResult) {
+          polylineCoordinates.value.add(
+            LatLng(pointLatLng.latitude, pointLatLng.longitude),
+          );
+        }
+      }
+
+      // * CREATE POLYLINE
+      polylineSet.value.clear();
+      Polyline polyline = Polyline(
+        polylineId: const PolylineId('polylineId'),
+        color: AppColor.primary,
+        jointType: JointType.round,
+        points: polylineCoordinates.value,
+        width: 5,
+        startCap: Cap.roundCap,
+        endCap: Cap.roundCap,
+        geodesic: true,
+      );
+
+      polylineSet.value.add(polyline);
+
+      // * CREATE BOUND LATLNG TO FIT TWO MARKERS
+      LatLngBounds latLngBounds;
+      if (pickUpLatLng.latitude > dropOffLatLng.latitude &&
+          pickUpLatLng.longitude > dropOffLatLng.longitude) {
+        latLngBounds =
+            LatLngBounds(southwest: dropOffLatLng, northeast: pickUpLatLng);
+      } else if (pickUpLatLng.longitude > dropOffLatLng.longitude) {
+        latLngBounds = LatLngBounds(
+          southwest: LatLng(pickUpLatLng.latitude, dropOffLatLng.longitude),
+          northeast: LatLng(dropOffLatLng.latitude, pickUpLatLng.longitude),
+        );
+      } else if (pickUpLatLng.latitude > dropOffLatLng.latitude) {
+        latLngBounds = LatLngBounds(
+          southwest: LatLng(dropOffLatLng.latitude, pickUpLatLng.longitude),
+          northeast: LatLng(pickUpLatLng.latitude, dropOffLatLng.longitude),
+        );
+      } else {
+        latLngBounds =
+            LatLngBounds(southwest: pickUpLatLng, northeast: dropOffLatLng);
+      }
+
+      // * MOVE CAMERA TO FIT TWO MARKERS
+      newRideGoogleMapController
+          .animateCamera(CameraUpdate.newLatLngBounds(latLngBounds, 70));
+
+      // * CREATE MARKER
+      Marker pickUpMarker = Marker(
+        markerId: const MarkerId('pickUpId'),
+        position: pickUpLatLng,
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen),
+      );
+
+      Marker dropOffMarker = Marker(
+        markerId: const MarkerId('dropOffId'),
+        position: dropOffLatLng,
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
+      );
+
+      markerSet.value.add(pickUpMarker);
+      markerSet.value.add(dropOffMarker);
+
+      // * CREATE CIRCLE
+      Circle pickUpCircle = Circle(
+        circleId: const CircleId('pickUpId'),
+        fillColor: AppColor.primary,
+        center: pickUpLatLng,
+        radius: 12,
+        strokeWidth: 4,
+        strokeColor: AppColor.primary,
+      );
+
+      Circle dropOffCircle = Circle(
+        circleId: const CircleId('dropOffId'),
+        fillColor: AppColor.primary,
+        center: dropOffLatLng,
+        radius: 12,
+        strokeWidth: 4,
+        strokeColor: AppColor.primary,
+      );
+
+      circleSet.value.add(pickUpCircle);
+      circleSet.value.add(dropOffCircle);
+    }
+
     return WillPopScope(
       onWillPop: () async {
         // Cant go back
         return Future.value(false);
       },
       child: Scaffold(
-        appBar: AppBar(
-          elevation: 0,
-          centerTitle: true,
-          title: SizedBox(
-            width: 90,
-            child: Image.asset(
-              AppAsset.logoDeliveritText2,
-              fit: BoxFit.contain,
-            ),
+        appBar: !isShowGoogleMap.value
+            ? AppBar(
+                elevation: 0,
+                centerTitle: true,
+                title: SizedBox(
+                  width: 90,
+                  child: Image.asset(
+                    AppAsset.logoDeliveritText2,
+                    fit: BoxFit.contain,
+                  ),
+                ),
+                backgroundColor: Colors.white,
+              )
+            : null,
+        bottomSheet: Visibility(
+          visible: !isShowGoogleMap.value,
+          child: _buildBottomSheet(
+            context,
+            distance: rideDetails.distance,
+            totalPayment: rideDetails.totalPayment,
+            paymentMethod: rideDetails.paymentMethod,
+            isShowGoogleMap: isShowGoogleMap,
           ),
-          backgroundColor: Colors.white,
         ),
-        bottomSheet: _buildBottomSheet(
-          context,
-          distance: rideDetails.distance,
-          totalPayment: rideDetails.totalPayment,
-          paymentMethod: rideDetails.paymentMethod,
-        ),
-        body: SingleChildScrollView(
-          padding: EdgeInsets.fromLTRB(16, 4, 16, bottomSheetHeight + 16),
-          child: Column(
-            children: <Widget>[
-              //* Card Pickup Address
-              _buildCardAddress(
-                title: 'Alamat pengambilan',
-                userName: rideDetails.sender.name,
-                placeName: rideDetails.pickup.placeName!,
-                address: rideDetails.pickup.placeFormattedAddress!,
-                phoneNumber: rideDetails.sender.phoneNumber,
-                note: rideDetails.sender.note,
-              ),
-              const SizedBox(height: 24),
+        body: Visibility(
+          visible: isShowGoogleMap.value,
+          replacement: _buildDetailRideRequest(),
+          child: SafeArea(
+            child: Stack(
+              children: [
+                GoogleMap(
+                  mapType: MapType.normal,
+                  myLocationButtonEnabled: true,
+                  initialCameraPosition: initialCameraPosition,
+                  myLocationEnabled: true,
+                  compassEnabled: true,
+                  zoomControlsEnabled: false,
+                  markers: markerSet.value,
+                  polylines: polylineSet.value,
+                  circles: circleSet.value,
+                  onMapCreated: (controller) async {
+                    EasyLoading.show(status: 'Loading...');
 
-              //* Card Dropoff Address
-              _buildCardAddress(
-                title: 'Alamat pengiriman',
-                userName: rideDetails.receiver.name,
-                placeName: rideDetails.dropoff.placeName!,
-                address: rideDetails.dropoff.placeFormattedAddress!,
-                phoneNumber: rideDetails.receiver.phoneNumber,
-                note: rideDetails.receiver.note,
-              ),
-              const SizedBox(height: 24),
+                    // print('onMapCreated');
+                    controllerGoogleMap.complete(controller);
+                    newRideGoogleMapController = controller;
 
-              //* Card List Payload
-              _buildCardListPayload(payloads: rideDetails.payloads),
-              const SizedBox(height: 24),
+                    var currentLatLng = LatLng(
+                        currentLocation!.latitude, currentLocation!.longitude);
+                    var pickupLatLng = LatLng(rideDetails.pickup.latitude!,
+                        rideDetails.pickup.longitude!);
 
-              //* Card Vehicle
-              _buildCardVehicle(
-                vehicle: rideDetails.vehicle,
-                carrier: rideDetails.carrier,
-              ),
-            ],
+                    await getPlaceDirection(currentLatLng, pickupLatLng);
+
+                    // print('finish getPlaceDirection: ${markerSet.value}');
+                    EasyLoading.dismiss();
+                    isDirectionLoaded.value = true;
+                  },
+                )
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 
-  Container _buildBottomSheet(
-    BuildContext context, {
-    required double distance,
-    required double totalPayment,
-    required String paymentMethod,
-  }) {
+  SingleChildScrollView _buildDetailRideRequest() {
+    return SingleChildScrollView(
+      padding: EdgeInsets.fromLTRB(16, 4, 16, bottomSheetHeight + 16),
+      child: Column(
+        children: <Widget>[
+          //* Card Pickup Address
+          _buildCardAddress(
+            title: 'Alamat pengambilan',
+            userName: rideDetails.sender.name,
+            placeName: rideDetails.pickup.placeName!,
+            address: rideDetails.pickup.placeFormattedAddress!,
+            phoneNumber: rideDetails.sender.phoneNumber,
+            note: rideDetails.sender.note,
+          ),
+          const SizedBox(height: 24),
+
+          //* Card Dropoff Address
+          _buildCardAddress(
+            title: 'Alamat pengiriman',
+            userName: rideDetails.receiver.name,
+            placeName: rideDetails.dropoff.placeName!,
+            address: rideDetails.dropoff.placeFormattedAddress!,
+            phoneNumber: rideDetails.receiver.phoneNumber,
+            note: rideDetails.receiver.note,
+          ),
+          const SizedBox(height: 24),
+
+          //* Card List Payload
+          _buildCardListPayload(payloads: rideDetails.payloads),
+          const SizedBox(height: 24),
+
+          //* Card Vehicle
+          _buildCardVehicle(
+            vehicle: rideDetails.vehicle,
+            carrier: rideDetails.carrier,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container _buildBottomSheet(BuildContext context,
+      {required double distance,
+      required double totalPayment,
+      required String paymentMethod,
+      required ValueNotifier<bool> isShowGoogleMap}) {
     return Container(
       padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
       height: bottomSheetHeight,
@@ -146,7 +328,7 @@ class NotificationRidePage extends StatelessWidget {
                   onTap: () {
                     // todo: add function to accept ride request
                     audioPlayer.stop();
-                    checkAvailibilityOfRide(context);
+                    checkAvailibilityOfRide(context, isShowGoogleMap);
                   },
                 ),
               ],
@@ -515,7 +697,10 @@ class NotificationRidePage extends StatelessWidget {
     );
   }
 
-  void checkAvailibilityOfRide(BuildContext context) async {
+  void checkAvailibilityOfRide(
+    BuildContext context,
+    ValueNotifier<bool> isShowGoogleMap,
+  ) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     String userId = prefs.getString('userId')!;
     DatabaseReference rideRequestRef =
@@ -538,6 +723,7 @@ class NotificationRidePage extends StatelessWidget {
       rideRequestRef.set('accepted');
       if (context.mounted) {
         acceptRideRequest(context);
+        isShowGoogleMap.value = true;
       }
     } else if (theRideId == 'canceled') {
       Fluttertoast.showToast(
