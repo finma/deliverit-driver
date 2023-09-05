90070eb73336aaf4d2ddfbf9ac6610659b3bfe91
finma <firmanmaulana2403@gmail.com>
2023-09-05 11:47:43 +0700

    [ADD] current position

--- lib/config/map_config.dart
+++ lib/config/map_config.dart
@@ -1,9 +1,12 @@
 import 'package:assets_audio_player/assets_audio_player.dart';
+import 'package:geolocator/geolocator.dart';
 
 import '/models/driver.dart';
 
 String mapKey = 'AIzaSyAb0VVTubNOlT-M8p4L5FyZnJoR9unOnxs';
 String oldMapKey = 'AIzaSyDPaBgt_oedAQiUxlApwiPc9LCsx7W96pQ';
+
+Position? currentLocation;
 
 AssetsAudioPlayer audioPlayer = AssetsAudioPlayer();
 Driver driverInformation = Driver();
