465bad7c53bd39e039bf6bab1afbc09197dba1a5
finma <firmanmaulana2403@gmail.com>
2023-09-05 13:27:44 +0700

    [ADD] home screen stream subscription

--- lib/config/map_config.dart
+++ lib/config/map_config.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:assets_audio_player/assets_audio_player.dart';
 import 'package:geolocator/geolocator.dart';
 
@@ -6,6 +8,7 @@ import '/models/driver.dart';
 String mapKey = 'AIzaSyAb0VVTubNOlT-M8p4L5FyZnJoR9unOnxs';
 String oldMapKey = 'AIzaSyDPaBgt_oedAQiUxlApwiPc9LCsx7W96pQ';
 
+StreamSubscription<Position>? homeScreenStreamSubscription;
 Position? currentLocation;
 
 AssetsAudioPlayer audioPlayer = AssetsAudioPlayer();
