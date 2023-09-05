8adb74d992cf4b06c7407eb61c13795e45e68dba
finma <firmanmaulana2403@gmail.com>
2023-09-05 15:35:58 +0700

    [ADD] ride stream subscription

--- lib/config/map_config.dart
+++ lib/config/map_config.dart
@@ -9,6 +9,7 @@ String mapKey = 'AIzaSyAb0VVTubNOlT-M8p4L5FyZnJoR9unOnxs';
 String oldMapKey = 'AIzaSyDPaBgt_oedAQiUxlApwiPc9LCsx7W96pQ';
 
 StreamSubscription<Position>? homeScreenStreamSubscription;
+StreamSubscription<Position>? rideStreamSubscription;
 Position? currentLocation;
 
 AssetsAudioPlayer audioPlayer = AssetsAudioPlayer();
